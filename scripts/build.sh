039b7c913aa0eee4c4efa2aa0636073f1ec1eebe
Garrett Potts <gpotts@radiantblue.com>
2017-06-20 13:36:44 -0400

    Added install prefix

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ mkdir -p $BUILD_DIR
 
 pushd $BUILD_DIR
 #cmake -DCMAKE_INSTALL_PREFIX=$OSSIM_INSTALL_PREFIX ..
-cmake ..
+cmake .. -DCMAKE_INSTALL_PREFIX=$OSSIM_INSTALL_PREFIX
 
 if [ $? != 0 ] ; then
    echo ERROR Generating CMake build files
